d3463cefb8b964655ccb031c5aaf2718056e44fe
abrahamuchos <j.abraham29@gmail.com>
2024-07-26 17:51:25 -0400

    Add componet to add text, info section

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import './App.css';
 function App() {
 
   return (
-    <>
+    <div className='max-w-[1440px] mx-auto relative'>
       <Header/>
       <Hero/>
       <Services/>
-    </>
+    </div>
   )
 }
 
--- src/componets/Text.jsx
+++ src/componets/Text.jsx
@@ -18,16 +18,17 @@ export default function Text({title, description, cta, className}) {
 
 
   return (
-    <div className={`p-3 flex-col place-content-around ${className}
+    <div className={`p-3 px-5 flex-col place-content-around ${className}
         h-[475px] text-center
         sm:h-[250px] sm:p-5
-        md:h-auto md:my-0 md:p-10 md:text-start
-        lg:h-[420px] lg:p-20
+        md:h-auto md:my-0 md:p-10 md:text-center px-4
+        lg:h-[420px] lg:p-20 lg:text-start
         xl:px-[15%] xl:py-[25%]`}
     >
       <h1 className='font-Fraunces-900 text-2xl text-Very-dark-desaturated-blue mb-5
         sm:text-lg
-        lg:text-4xl'
+        lg:text-3xl
+        xl:text-4xl'
       >
         {title}
       </h1>
@@ -42,7 +43,7 @@ export default function Text({title, description, cta, className}) {
           {cta.label}
         </a>
         <div className={`absolute w-[110px] h-[10px] rounded-full  -z-10 opacity-35 left-0 right-0 mx-auto top-[15px]
-          md:left-0 md:mx-0 
+          lg:left-0 lg:mx-0 
           ${cta.bgColor ?? ''}`}
         >
         </div>
